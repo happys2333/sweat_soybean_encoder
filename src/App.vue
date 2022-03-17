5320908a3de7e96f23c3d82d8aba7a3b000f7153
happys <happys2333@outlook.com>
2022-03-17 19:18:03 +0800

    😅 init elementUI
    😅差不多得了

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <el-input
+      v-model="textarea"
+      :rows="2"
+      type="textarea"
+      placeholder="Please input"
+  />
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
